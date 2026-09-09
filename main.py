93acc95dae90d28b5c6fb909dc264b77bf256054
paul <paulestrella92@gmail.com>
2026-09-08 21:46:55 -0400

    agregamos opcion 4 para ver disponibilidad de productos

--- main.py
+++ main.py
@@ -22,12 +22,21 @@ def agregar_producto(catalogo, nombre, precio, disponible):
     return nuevo_producto
 
 
+def producto_disponible(catalogo, disponible):
+    productos_disponibles = []
+    for producto in catalogo:
+        if producto["disponibilidad"] == disponible:
+            productos_disponibles.append(producto)
+    return productos_disponibles
+
+
 def main():
     while True:
         print("0. Salir")
         print("1. Ver catalogo completo")
         print("2. Buscar un producto")
         print("3. Agregar un producto nuevo")
+        print("4. Ver solo los productos disponibles")
         opcion = input("Elige una opcion: ")
 
         if opcion == "0":
@@ -52,6 +61,11 @@ def main():
             disponible = True
             producto = agregar_producto(catalogo_negocio, nombre, precio, disponible)
             print(f"Se agrego el producto: {producto['nombre']} - ${producto['precio']}")
+
+        elif opcion == "4":
+            productos = producto_disponible(catalogo_negocio, True)
+            for producto in productos:
+                print(f"{producto['nombre']}: ${producto['precio']}")
 
 
 if __name__ == "__main__":
