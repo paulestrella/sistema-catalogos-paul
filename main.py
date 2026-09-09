1e619bb036b4ef8421cdd9bd3b4f064937340d9c
paul <paulestrella92@gmail.com>
2026-09-08 20:56:50 -0400

    agregamos opcion de ver catalogo completo

--- main.py
+++ main.py
@@ -8,11 +8,16 @@ catalogo_negocio = [
 def main():
     while True:
         print("0. Salir")
+        print("1. Ver catalogo completo")
         opcion = input("Elige una opcion: ")
 
         if opcion == "0":
             print("Hasta luego!")
             break
+
+        elif opcion == "1":
+            for producto in catalogo_negocio:
+                print(f"{producto['nombre']}: ${producto['precio']}")
 
 
 if __name__ == "__main__":
